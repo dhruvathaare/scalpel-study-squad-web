b2069372fa5e4a68db7fcf4172c9d25cdc66d9ce
dhruvathaare <143250636+dhruvathaare@users.noreply.github.com>
2026-02-13 16:58:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'scalpel-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.url).catch(() => caches.match(event.request))
+    );
 });
