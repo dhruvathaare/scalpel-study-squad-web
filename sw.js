1320af3ddfac5d0b82ce05785ec8479ffa66356a
dhruvathaare <143250636+dhruvathaare@users.noreply.github.com>
2026-02-13 17:03:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
-const CACHE_NAME = 'scalpel-v1';
-const assets = ['./', './index.html', './manifest.json'];
-
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(assets);
-        })
-    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.url).catch(() => caches.match(event.request))
-    );
+    // This allows the app to load normally while satisfying Chrome's PWA rules
+    event.respondWith(fetch(event.request));
 });
